19a3472ae9f8ee31d58717627a7db9d163d26bfd
Gedalya <gedalya@krycer.com>
2022-06-11 12:55:14 -0700

    Refactor context to `useContext`

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { ThemeProvider } from "./contexts/theme";
+import { ThemeContext } from "./contexts/theme";
 import Nav from "./components/Nav";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Loading from "./components/Loading";
@@ -19,7 +19,7 @@ function App() {
 
   return (
     <Router>
-      <ThemeProvider value={{ theme, toggleTheme }}>
+      <ThemeContext.Provider value={{ theme, toggleTheme }}>
         <div className={theme}>
           <div className="container">
             <Nav />
@@ -34,7 +34,7 @@ function App() {
             </React.Suspense>
           </div>
         </div>
-      </ThemeProvider>
+      </ThemeContext.Provider>
     </Router>
   );
 }
